751115d42b0f4681829973e6b663748abea4c154
Andrew Belyaev <abelyaev.vmk@gmail.com>
2017-05-30 00:33:03 +0300

    Update detection pipeline

--- Task3/exps/face_detection.sh
+++ Task3/exps/face_detection.sh
@@ -5,7 +5,7 @@ IMAGES_DIR='data/images_from_video'
 LOGS='exps/faces/logs.txt'
 
 echo "Creating images"
-python3 videomodul.py --create-images --path=${VIDEO_NAME} --out=${IMAGES_DIR}
+python3 ./tools/videomodul.py --create-images --path=${VIDEO_NAME} --out=${IMAGES_DIR}
 echo "Start CNN"
 python3 ./tools/test_net.py --gpu 0 \
     --net models/faces/faces.caffemodel \
@@ -14,4 +14,4 @@ python3 ./tools/test_net.py --gpu 0 \
     --exp_dir faces > ${LOGS}
 VIDEOSET=`cat ${LOGS} | grep "Output detections file: " | cut -f4- -d" "`
 echo "Creating video"
-python3 videomodul.py --create-video --path=${IMAGES_DIR} --marking=${VIDEOSET} --out=result_video.avi
+python3 ./tools/videomodul.py --create-video --path=${IMAGES_DIR} --marking=${VIDEOSET} --out=result_video.avi
